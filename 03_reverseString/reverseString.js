ca8fa2a7115daa39173e9268c9553d30e8051491
Connor Schotz <140295366+Packstone01@users.noreply.github.com>
2024-10-14 17:21:18 -0400

    Finished the reverse string practice.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let split = str.split('');
 
+  let reverse = split.reverse();
+
+  let join = reverse.join('');
+
+  return join;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
